862387efae38d7863a31f6b5eb48252c603486da
Rodrigo Bunhak <rodrigo.bunhak@gmail.com>
2022-03-27 12:02:42 -0300

    add class abstract calculomatematico

---
+++ classes/CalculoMatematico.java
@@ -0,0 +1,31 @@
+package classes;
+
+import interfaces.CalculadorMatematico;
+
+public abstract class CalculoMatematico implements CalculadorMatematico {
+
+  private int numero1;
+  private int numero2;
+
+  public CalculoMatematico(int numero1, int numero2) {
+    this.numero1 = numero1;
+    this.numero2 = numero2;
+  }
+
+  @Override
+  public Boolean validar() {
+    return numero1 >= 0 && numero2 >= 0;
+  }
+
+  @Override
+  public final int calcular() {
+    if (validar()) {
+      return doCalcular();
+    } else {
+      return -1;
+    }
+  }
+
+  protected abstract int doCalcular();
+  
+}
